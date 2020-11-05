5b8c25343b7ef7c839c2eb528c097febcd428fc0
CRoulleau <zerodegreedrinky@gmail.com>
2020-11-05 08:42:10 +0100

    mise en place mvc

---
+++ views/accueil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <title>Blog Jean Forteroche</title>
+        <meta charset="utf-8" >
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
+		<meta name="description" content="" />
+        <link rel="stylesheet" href="style.css" >
+         
+     
+    </head>
+    <body>
+        <div id="bloc_page">
+        <header>                
+            <h1>Blog de Jean Forteroche</h1>
+            <nav>
+        <ul>            
+            <li><a href="#">Accueil</a> </li>
+            <li><a href="#">Contact</a> </li>           
+        </ul>        
+        </nav>
+            </header>
+           
+            <section>
+                
+            </section>
+            <footer>
+                <p>Copyright 2020 Claire Roulleau</p>        
+            </footer>
+        </div><!--fin bloc_page-->       
+       
+    </body>
+</html>
